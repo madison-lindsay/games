e2fa84c33d92aef613b94772d06b65293f5a3697
madison-lindsay <madison.lindsay@richmond.edu>
2022-11-11 09:17:01 -0500

    added validate_guess to ensure guesses are in the correct format

--- mastermind.py
+++ mastermind.py
@@ -1,8 +1,36 @@
 import numpy as np
+import sys
+
 
 def generate_solution(size):
     sol = np.random.choice(range(size+2), size=size)
     return sol
+
+
+def validate_guess(guess, size):
+    return_true = True
+    user_input = [x for x in guess.split(" ") if x != ""]
+    if len(user_input) > size:
+        print(f'You have entered {len(user_input)} values! Only {size} are permitted. Try again.')
+        return_true = False
+
+    return_guess = []
+    for x in user_input:
+        try:
+            to_append = int(x)
+            if (to_append > size + 1) or (to_append < 0):
+                print(f'Input {x} is not in the specified range! Try again.')
+                return_true = False
+            else:
+                return_guess.append(to_append)
+        except:
+            print(f'Input {x} is not an integer! Try again.')
+            return_true = False
+        if not return_true:
+            return False
+
+    return return_guess
+
 
 def evaluate_guess(guess, sol, size):
     response = []
@@ -28,13 +56,16 @@ def evaluate_guess(guess, sol, size):
         response.append("❌")
     return response, len(guess) == 0
 
+
 def main(size=4):
     sol = list(generate_solution(size))
     tries = 0
     victory = False
     while not victory:
-        guess = input("Guess {0} digits between zero and {1}: ".format(size, size+1))
-        guess = [int(x) for x in guess if x != " "]
+        guess = input("Guess {0} integers from zero to {1}: ".format(size, size+1))
+        guess = validate_guess(guess, size)
+        if not guess:
+            continue
         response, victory = evaluate_guess(guess, sol, size)
         tries += 1
         for symb in response:
@@ -42,4 +73,9 @@ def main(size=4):
         print("")
     print("VICTORY in {} tries".format(tries))
 
-main()
+
+if len(sys.argv) > 1:
+    size = int(sys.argv[1])
+    main(size=size)
+else:
+    main()
